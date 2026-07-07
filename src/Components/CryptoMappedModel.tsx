0264183bed92e4e2d57c86a5c55a3d997ae76ffe
Szymon Adamczyk <sadamczyk9@ucn.zsem.edu.pl>
2026-07-07 19:41:22 +0200

    fix: fixing brightness attriboute

--- src/Components/CryptoMappedModel.tsx
+++ src/Components/CryptoMappedModel.tsx
@@ -8,9 +8,7 @@ function ProceduralCoin({ textureUrl }: props) {
 
     return (
         <mesh rotation={[Math.PI / 2, 0, 0]}>
-            {/* cylinderGeometry args = [promień góra, promień dół, grubość monety, liczba segmentów] */}
             <cylinderGeometry args={[2, 2, 0.3, 32]} />
-            {/* Nakładamy teksturę na nasz wygenerowany cylinder */}
             <meshStandardMaterial map={texture} roughness={0.3} metalness={0.8} />
         </mesh>
     );
